dee924764acff95905f8c1e2071fcaeb2815cc73
ImadAbid <imad.abied@gmail.com>
2021-05-11 16:52:24 +0200

    moving window integration

---
+++ src/moving_window_intergration.m
@@ -0,0 +1,10 @@
+function sigI = moving_window_intergration(sig,rec_win_len)
+    sigI = zeros(1, length(sig));
+    for i= 1:1:length(sig)
+        di = 0;
+        while(i-di > 0 && di < rec_win_len)
+            sigI(i) = sigI(i) + sig(i-di); 
+            di = di + 1;
+        end
+    end
+end
