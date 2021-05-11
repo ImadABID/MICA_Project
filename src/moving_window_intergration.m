ff83b6ae03a9401b833daa5ed7e74b773b1ca21d
Bigi-Med <medbigi2000@gmail.com>
2021-05-11 16:57:55 +0200

    dividing by window length

--- src/moving_window_intergration.m
+++ src/moving_window_intergration.m
@@ -6,5 +6,6 @@ function sigI = moving_window_intergration(sig,rec_win_len)
             sigI(i) = sigI(i) + sig(i-di); 
             di = di + 1;
         end
+        sigI(i) = sigI(i)/rec_win_len;
     end
 end
