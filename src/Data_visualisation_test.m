5ee328b1a0dd5586aa4612a7b5679d65b5d15e66
Bigi-Med <medbigi2000@gmail.com>
2021-05-11 18:02:33 +0200

    ploting ECG in differents states

--- src/Data_visualisation_test.m
+++ src/Data_visualisation_test.m
@@ -1,3 +1,7 @@
+clc;
+clear all;
+close all;
+
 Sig = load("../data/ecg_VF.mat");
 
 N_fft = 1024;
@@ -6,3 +10,5 @@ d = 10;
 window = hann(N);
 
 spectro(Sig.ecg, window,d,N_fft,Sig.Fs);
+
+R_locs = wave_detection_R(Sig.ecg,1/(Sig.Fs),2000,40);
