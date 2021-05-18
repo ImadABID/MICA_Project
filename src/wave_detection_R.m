6231074c0567ad4440043df62ca33e3c31496b5f
ImadAbid <imad.abied@gmail.com>
2021-05-18 18:10:22 +0200

    Cleanning src/wave_detection_R.m function

--- src/wave_detection_R.m
+++ src/wave_detection_R.m
@@ -1,36 +1,46 @@
 function [R_locations] = wave_detection_R(ECG,Ts,window_length)
 
+%% Passe bas
 coeff_nume_bas = conv([1 0 0 0 0 0 -1],[1 0 0 0 0 0 -1]); %les coefficients du numérateur de la transformeé en z du filter passe_bas
 coeff_denom_bas = [1 -2 1];
-%ECG = [[0 0 ] ,ECG,[0 0]];
-y_bas=filter(coeff_nume_bas,coeff_denom_bas,ECG);
-%y_bas = [[0 0 ], y_bas,[0 0]];
 
+y_bas=filter(coeff_nume_bas,coeff_denom_bas,ECG);
+
+%% Passe haut
 coeff_nume_haut = zeros(1,33);%les coefficients du numérateur de la transformeé en z du filter passe_bas
 coeff_nume_haut(1,1)=-1;
 coeff_nume_haut(1,17)=32;
 coeff_nume_haut(1,18)=-32;
 coeff_nume_haut(1,33)=1;
-
 coeff_denom_haut =[1,-1];
 
-y_final=filter(coeff_nume_haut,coeff_denom_haut,y_bas);
+sig_band_pass=filter(coeff_nume_haut,coeff_denom_haut,y_bas);
 retard_pass_bande = 20;
-y_final = y_final(retard_pass_bande:1:length(y_final));
-y_derivated= filter([1,2,0,-2,-1],[1],y_final).*(1/(8*Ts));
+sig_band_pass = sig_band_pass(retard_pass_bande:1:length(sig_band_pass));
+
+%% Derivation
+y_derivated= filter([1,2,0,-2,-1],[1],sig_band_pass).*(1/(8*Ts));
 retard_derivation = 10;
 
-S_sq = abs(y_derivated(10:1:length(y_derivated))).^2;
+%% Squaring
+S_sq = abs(y_derivated(retard_derivation:1:length(y_derivated))).^2;
 S_mwi = moving_window_intergration(S_sq,window_length);
+
+%% Thresholding
 thresholding_step = thresholding(S_mwi);
+
+%% Detecting R
+fprintf("length(thresholding_step) : %d\n", length(thresholding_step));
+fprintf("length(sig_band_pass) : %d\n", length(sig_band_pass));
+
+%signal_to_extract_R = thresholding_step .* sig_band_pass;
+
 
 R_locations = zeros(1,100);
 
 figure,
-nb_values = 6000;
+nb_values = 1000;
 nbr_figures = 6;
-
-%time_ecg = linspace(1,Ts,X);
 
 subplot(5,1,1);
 plot(ECG(1:nb_values));
@@ -40,14 +50,14 @@ ylabel("ECG en mV")
 grid on ;
 
 subplot(nbr_figures,1,2);
-plot(y_final(1:nb_values));
+plot(sig_band_pass(1:nb_values));
 title('ECG after band-pass filtre');
 xlabel("time")
 ylabel("magnitude")
 grid on ;
 
 subplot(nbr_figures,1,3);
-plot(y_derivated(10:nb_values));
+plot(y_derivated(1:nb_values));
 title('ECG Derivated');
 xlabel("time")
 ylabel("magnitude")
