c24d2982ed0421f62bd9c17d56ab898aa3eb2f53
Bigi-Med <medbigi2000@gmail.com>
2021-05-18 16:39:11 +0200

    fixed filter delays

--- src/wave_detection_R.m
+++ src/wave_detection_R.m
@@ -2,9 +2,9 @@ function [R_locations] = wave_detection_R(ECG,Ts,nb_values,window_length)
 
 coeff_nume_bas = conv([1 0 0 0 0 0 -1],[1 0 0 0 0 0 -1]); %les coefficients du numérateur de la transformeé en z du filter passe_bas
 coeff_denom_bas = [1 -2 1];
-
+%ECG = [[0 0 ] ,ECG,[0 0]];
 y_bas=filter(coeff_nume_bas,coeff_denom_bas,ECG);
-
+%y_bas = [[0 0 ], y_bas,[0 0]];
 
 coeff_nume_haut = zeros(1,33);%les coefficients du numérateur de la transformeé en z du filter passe_bas
 coeff_nume_haut(1,1)=-1;
@@ -15,10 +15,10 @@ coeff_nume_haut(1,33)=1;
 coeff_denom_haut =[1,-1];
 
 y_final=filter(coeff_nume_haut,coeff_denom_haut,y_bas);
-
+y_final = y_final(20:1:length(y_final));
 y_derivated= filter([1,2,0,-2,-1],[1],y_final).*(1/(8*Ts));
 
-Ssq = abs(y_derivated(1:1:length(y_derivated))).^2;
+Ssq = abs(y_derivated(10:1:length(y_derivated))).^2;
 
 R_locations = zeros(1,100);
 
@@ -27,27 +27,32 @@ figure,
 
 %time_ecg = linspace(1,Ts,X);
 
-subplot(4,1,1);
+subplot(5,1,1);
 
 %plot(time_ecg,ECG);
 plot(ECG(1:nb_values));
 title('ECG');
 grid on ;
 
-subplot(4,1,2);
+subplot(5,1,2);
 plot(y_final(1:nb_values));
 title('ECG pass bande');
 grid on ;
-subplot(4,1,3);
+subplot(5,1,3);
+plot(y_derivated(10:nb_values));
+title('ECG Derivated');
+grid on;
+subplot(5,1,4);
 plot(Ssq(1:nb_values));
 title('ECG module carre');
 grid on;
-subplot(4,1,4);
+subplot(5,1,5);
 grid on ;
 X = moving_window_intergration(Ssq,window_length);
 plot(X(1:nb_values));
 title('ECG MVI');
 grid on ;
+
 
 end
 
