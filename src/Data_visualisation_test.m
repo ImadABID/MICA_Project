511f8fe905e75519ee4cffef8cf13790c9879bea
Bigi-Med <medbigi2000@gmail.com>
2021-05-19 11:03:30 +0200

    problrmed buggin fixed

--- src/Data_visualisation_test.m
+++ src/Data_visualisation_test.m
@@ -5,7 +5,7 @@ close all;
 Sig = load("../data/ecg_normal_1.mat");
 
 % Adapting ECG polarisation
-[max, max_index] = max(abs(Sig.ecg));
+[maxi, max_index] = max(abs(Sig.ecg));
 if Sig.ecg(max_index) < 0
     Sig.ecg = -Sig.ecg;
 end
@@ -19,3 +19,5 @@ window = hann(N);
 spectro(Sig.ecg, window,d,N_fft,Sig.Fs);
 
 R_locs = wave_detection_R(Sig.ecg,1/(Sig.Fs), 40);
+
+T_locs = wave_detection_T(Sig.ecg,[1 100 200 300 600 900]);
--- src/wave_detection_R.m
+++ src/wave_detection_R.m
@@ -17,14 +17,14 @@ coeff_denom_haut =[1,-1];
 sig_band_pass=filter(coeff_nume_haut,coeff_denom_haut,y_bas);
 
 retard_pass_bande = delay_between_2_sigs(sig_band_pass, y_bas);
-fprintf("Delay band-pass : %d\n", retard_pass_bande);
+%fprintf("Delay band-pass : %d\n", retard_pass_bande);
 ECG = ECG(1:1:length(ECG)-retard_pass_bande);
 sig_band_pass = sig_band_pass(retard_pass_bande:1:length(sig_band_pass));
 
 %% Derivation
 y_derivated= filter([1,2,0,-2,-1], [1], sig_band_pass).*(1/(8*Ts));
 retard_derivation = delay_between_2_sigs(y_derivated, sig_band_pass);
-fprintf("Delay derivation : %d\n", retard_derivation);
+%fprintf("Delay derivation : %d\n", retard_derivation);
 ECG = ECG(1:1:length(ECG)-retard_derivation);
 sig_band_pass = sig_band_pass(1:1:length(sig_band_pass)-retard_derivation);
 y_derivated = y_derivated(retard_derivation:1:length(y_derivated));
@@ -37,8 +37,8 @@ S_mwi = moving_window_intergration(S_sq,window_length);
 thresholding_step = thresholding(S_mwi);
 
 %% Detecting R
-fprintf("length(thresholding_step) : %d\n", length(thresholding_step));
-fprintf("length(sig_band_pass) : %d\n", length(sig_band_pass));
+%fprintf("length(thresholding_step) : %d\n", length(thresholding_step));
+%fprintf("length(sig_band_pass) : %d\n", length(sig_band_pass));
 
 %signal_to_extract_R = thresholding_step .* sig_band_pass;
 
